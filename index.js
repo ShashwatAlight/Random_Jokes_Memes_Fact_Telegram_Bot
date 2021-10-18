03f6cc50d3c96119e6444117af64da79c347d5a3
Shashwat <tamit3599@gmail.com>
2021-10-18 21:43:30 +0530

    procfile adding

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf');
 const axios = require('axios');
-require('dotenv').config()
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
